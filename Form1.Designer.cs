465a9a7e6c0f4c2c144521125b16a5f9de5ebec3
rus_anonym <alexandrsemin2033@gmail.com>
2021-03-16 15:21:04 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ClickButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ClickButton
+            // 
+            this.ClickButton.Image = ((System.Drawing.Image)(resources.GetObject("ClickButton.Image")));
+            this.ClickButton.Location = new System.Drawing.Point(350, 194);
+            this.ClickButton.Name = "ClickButton";
+            this.ClickButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ClickButton.Size = new System.Drawing.Size(160, 144);
+            this.ClickButton.TabIndex = 0;
+            this.ClickButton.UseVisualStyleBackColor = true;
+            this.ClickButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 586);
+            this.Controls.Add(this.ClickButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClickButton;
+    }
+}
+
