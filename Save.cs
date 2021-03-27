6fec54f26a2b80a71dd8177ea6e9ecab0df713e2
rus_anonym <alexandrsemin2033@gmail.com>
2021-03-28 02:14:56 +0300

    add comments to Save.cs

--- Save.cs
+++ Save.cs
@@ -1,10 +1,10 @@
 ﻿namespace Clicker
 {
-    public readonly struct UpgradeSave
+    public readonly struct UpgradeSave // Предоставляет структуру для хранения данных об улучшении
     {
-        public readonly int Price;
-        public readonly int Value;
-        public readonly UpgradeType Type;
+        public readonly int Price; // Отвечает за цену улучшения
+        public readonly int Value; // Отвечает за значение улучшения
+        public readonly UpgradeType Type; // Отвечает за тип улучшения
 
         public UpgradeSave(int price, int value, UpgradeType type)
         {
@@ -14,12 +14,12 @@
         }
     }
 
-    public readonly struct Save
+    public readonly struct Save // Предоставляет структуру для хранения данных об сохранении
     {
-        public readonly int Score;
-        public readonly int ScorePerClick;
-        public readonly int ScorePerSecond;
-        public readonly UpgradeSave[] Upgrades;
+        public readonly int Score; // Количество очков
+        public readonly int ScorePerClick; // Количество очков за клик
+        public readonly int ScorePerSecond; // Количество очков за секунду
+        public readonly UpgradeSave[] Upgrades; // Массив с структурой данных улучшений
 
         public Save(int score, int scorePerClick, int scorePerSecond, UpgradeSave[] upgrades)
         {
